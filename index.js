3a616fe13c251d3901f6662b1ae87144a5f54d95
Tai Dong <dongtaiit@gmail.com>
2020-01-18 15:59:22 +0700

    init proj

---
+++ index.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+let Promise = require('bluebird')
+
+
+let delay = async(timeout)=>{
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            resolve()
+        }, timeout)
+    })
+}
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 5,
+  });
+  const page = await browser.newPage();
+  await page.goto('https://youtube.com');
+  await page.type('#search', process.env.link);
+//   await Promise.delay(5000)
+
+  await page.click('button[id=search-icon-legacy]')
+
+  await page.waitForXPath("//div[@id='container']/ytd-two-column-search-results-renderer[@class='style-scope ytd-search']/div[@id='primary']/ytd-section-list-renderer[@class='style-scope ytd-two-column-search-results-renderer']/div[@id='contents']")
+    .then(()=> delay(1000))
+
+  let selector = "a#video-title"
+  await (await page.$(selector)).click().then( () => delay(3000)) 
+
+  while (true) {
+    let matrixValue = await page.evaluate(() => window.getComputedStyle(document.querySelector('.ytp-play-progress')).transform)
+    let endTimeFlag = matrixValue.slice(6).split(',')[0].slice(1)
+    if (endTimeFlag == 1) {
+      let replayBtn = "button.ytp-play-button"
+      await (await page.$(replayBtn)).click()      
+    }
+  }
+//   await browser.close();
+})();
+
